eea36c8cb308b593715c620bd25787eaf0f121b5
Fotikhjon <muhammad2004fotih@gmail.com>
2023-08-15 10:52:33 +0500

    First commit only with main

---
+++ src/main/java/uz/supersite/super_site_backend/SuperSiteBackEndApplication.java
@@ -0,0 +1,13 @@
+package uz.supersite.super_site_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperSiteBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperSiteBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/supersite/super_site_backend/SuperSiteBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.supersite.super_site_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperSiteBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
